99157ed83f0b6d673d5a4ffd9e68c20f8353e817
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-19 09:01:34 +0700

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -1,6 +1,5 @@
 import os
 
-from telethon import TelegramClient
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
@@ -19,6 +18,15 @@ ASSISTANT_ID = Config.ASSISTANT_ID
 bot = TelegramClient('Zaid', api_id=Config.API_ID, api_hash=Config.API_HASH)
 Zaid = bot.start(bot_token=Config.BOT_TOKEN)
 client = TelegramClient(StringSession(Config.STRING_SESSION), Config.API_ID, Config.API_HASH)
-call_py = PyTgCalls(client)
 client.start()
+call_py = PyTgCalls(client)
+test_stream = 'http://docs.evostream.com/sample_content/assets/' \
+              'sintel1m720p.mp4'
+call_py.play(
+chat.id,
+    MediaStream(
+        test_stream,
+    ),
+)
+idle()
 call_py.start()
