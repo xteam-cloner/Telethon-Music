c9add402cd6956b41bb3f1f084ff30a841b2823e
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-07 22:32:07 +0700

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -24,11 +24,16 @@ Zaid = bot.start(bot_token=Config.BOT_TOKEN)
 client = TelegramClient(StringSession(Config.STRING_SESSION), Config.API_ID, Config.API_HASH)
 client.start()
 
-call_py = TelegramClient(
-    'py-tgcalls',
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-)
+class call_py:
+    def __init__(self):
+        self.calls = []
+
+        for client in userbot.clients:
+            pycall = PyTgCalls(
+                client,
+                cache_duration=100,
+            )
+            self.calls.append(pycall)
 
 test_stream = 'http://docs.evostream.com/sample_content/assets/' \
               'sintel1m720p.mp4'
