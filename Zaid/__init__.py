5fbe9b6e2fb6886c8bdf0936df7c481f1cebc974
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-07 22:34:02 +0700

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -37,7 +37,7 @@ class call_py:
 
 test_stream = 'http://docs.evostream.com/sample_content/assets/' \
               'sintel1m720p.mp4'
-call_py.play(
+call_py(
 chat.id,
     MediaStream(
         test_stream,
