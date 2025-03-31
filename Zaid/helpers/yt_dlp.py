2fa9d4814d5de4a1eabb4454109f8ac9bae7c8f5
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-31 11:36:44 +0700

    Update yt_dlp.py

--- Zaid/helpers/yt_dlp.py
+++ Zaid/helpers/yt_dlp.py
@@ -8,7 +8,7 @@ import os.path
 from PIL import Image
 from yt_dlp import YoutubeDL
 from typing import Optional, Union
-from Zaid import Zaid as bot
+from Zaid import bot
 LOGS = {}
 SUDO_USERS = {}
 
